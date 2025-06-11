207732c53cabbd592992b21bf2863112c0474f24
Wesley Anjos <wess@SrWess.localdomain>
2025-06-11 09:00:05 -0300

    chore: estrutura inicial do backend

---
+++ backend/src/server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const apiRoutes = require('../routes');
+
+const PORT = 3001;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', apiRoutes);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server is running on port ${PORT}`);
+});
