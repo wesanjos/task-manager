ad65b8953e42aec6f606330bac2360dde7b9d089
Wesley Anjos <wanjos.andrade@gmail.com>
2025-06-12 08:29:14 -0300

    chore: setup inicial de testes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.js',
+    css: true,
+    reporters: ['verbose'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*'],
+      exclude: [],
+    },
+  },
 });
